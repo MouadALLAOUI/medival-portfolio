461b33f7f40fa354c679a184bb11a783fc1213f7
mouad ALLAOUI <96632243+MouadALLAOUI@users.noreply.github.com>
2025-11-04 17:49:26 +0100

    Update environment configuration for production

--- env.js
+++ env.js
@@ -1,16 +1,17 @@
 const ENV = {
   PROD: {
-    API_URL: "",
-    BACKEND_URL: "",
+    API_URL: "https://medival-portfolio-apis.yzz.me/api",
+    BACKEND_URL: "https://medival-portfolio-apis.yzz.me",
+    API_KEY: 'MEDIEVAL_API_KEY_7c24b777ad2c13880bbfa627d4db70cc' // test api key
   },
   DEV: {
     API_URL: 'http://localhost/medival-portfolio-apis/api',
     BACKEND_URL: 'http://localhost/medival-portfolio',
-    API_KEY: 'MEDIEVAL_API_KEY_12345'
+    API_KEY: 'MEDIEVAL_API_KEY_12345' // test api key
   },
 };
 
-const APP_ENV = "DEV";
+const APP_ENV = "PROD";
 
 export default function GET_ENV() {
   return ENV[APP_ENV];
